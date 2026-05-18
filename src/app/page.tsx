54111a3da2eb7ed48989f60f5748d569da7fc200
Yeu <yeumensilva9@gmail.com>
2026-05-18 16:18:10 -0300

    Redirection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
-"use client";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
-const Home = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push("/login");
-  }, []);
+// app/page.tsx
+export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">¡Bienvenido a tu Biblioteca!</h1>
+      <p className="mt-4">Si ves esto, estás logueado correctamente.</p>
+    </main>
   );
-};
-
-export default Home;
+}
